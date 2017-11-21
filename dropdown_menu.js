e2c0afefabdd7a9876e2af1e4ee9e845598953b8
jasonplojo <jason.plojo@gmail.com>
2017-11-20 19:00:46 -0800

    Implemented preliminary drop down menu for content area

---
+++ dropdown_menu.js
@@ -0,0 +1,5 @@
+// Function that will toggle the dropdown-show style for the menu item
+// passed, that is defined in "style.css".
+function showContent(menuItem) {
+  document.getElementById(menuItem).classList.toggle("dropdown-show");
+}
