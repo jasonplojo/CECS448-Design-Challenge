b5d3db2e9f58d14bc9e1d1064336eb2dd1d9084c
jasonplojo <jason.plojo@gmail.com>
2017-11-22 01:05:52 -0800

    Implemented dropdown menus for content section TODO: Comment my code

--- dropdown_menu.js
+++ dropdown_menu.js
@@ -1,5 +1,8 @@
 // Function that will toggle the dropdown-show style for the menu item
 // passed, that is defined in "style.css".
-function showContent(menuItem) {
-  document.getElementById(menuItem).classList.toggle("dropdown-show");
+function showContent(menuItem, buttonItem) {
+  document.getElementById(menuItem).classList.toggle("dropdown-hide");
+
+  document.getElementById(buttonItem).classList.toggle("triangle-up");
+  document.getElementById(buttonItem).classList.toggle("triangle-down");
 }
